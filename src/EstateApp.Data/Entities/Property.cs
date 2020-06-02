f211adc0c9795dca71c246e48e44f80e682bdff8
C33jay <elijahchijioke,ce@gmail.com>
2020-06-02 10:22:00 +0100

    can add and run migrations

--- src/EstateApp.Data/Entities/Property.cs
+++ src/EstateApp.Data/Entities/Property.cs
@@ -4,5 +4,6 @@ namespace EstateApp.Date.Entities
 {
     public class Property : BaseEntity
     {
-        public string Title { get; set;} //# 3 bedroom flat in Abule Egba    }
+        public string Title { get; set;} //# 3 bedroom flat in Abule Egba   
+    }
 }
